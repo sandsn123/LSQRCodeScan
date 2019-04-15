654197d10e00638491913271414a06ca460c5d09
李赛 <3390740015@qq.com>
2019-04-15 09:36:36 +0800

    add shared gramework

--- QRCodeScan/CreatQRcodeViewController.swift
+++ QRCodeScan/CreatQRcodeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import LSQRCodeScanTool
 
 class CreatQRcodeViewController: UIViewController {
 
--- QRCodeScan/ScanDemoViewController.swift
+++ QRCodeScan/ScanDemoViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Photos
+import LSQRCodeScanTool
 
 class ScanDemoViewController: UIViewController {
     
